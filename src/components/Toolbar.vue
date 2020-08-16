40a18c097b431709c2ba19093fda8efc2cc53bd1
Declan Murphy <declxn@gmail.com>
2020-08-16 09:35:36 -0500

    Initial Commit

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,7 @@
+<style>
+  <div class="toolbar">
+    <button class="toolbar-button">New</button>
+    <button class="toolbar-button">Delete</button>
+    <input class="toolbar-search" type="text" placeholder="Search...">
+  </div>
+</style>
